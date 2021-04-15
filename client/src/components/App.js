b9c04aa123b30745d0ecb5f5bb34616b5821d062
tpo-tcmurphy <77013859+tpo-tcmurphy@users.noreply.github.com>
2021-04-15 16:32:58 -0400

    Updated User Signout

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -15,13 +15,13 @@ import Header from './Header'
 import Courses from './Courses'
 import CourseDetail from './CourseDetail'
 import UserSignIn from './UserSignIn'
-
+import UserSignOut from './UserSignOut'
 // Components with Context
 import withContext from '../Context'
 const HeaderWithContext = withContext(Header)
 const CourseDetailWithContext = withContext(CourseDetail)
 const UserSignInWithContext = withContext(UserSignIn)
-
+const UserSignOutWithContext = withContext(UserSignOut)
 // NOTE: reset.css is being imported in index.html - remove if not needed (clashes with index.css??)
 
 function App () {
@@ -33,6 +33,7 @@ function App () {
           <Route exact path='/' render={() => <Courses />} />
           <Route exact path='/courses/:id' component={CourseDetailWithContext} />
           <Route path='/signin' component={UserSignInWithContext} />
+          <Route path='/signout' component={UserSignOutWithContext} />
         </Switch>
       </BrowserRouter>
 
--- client/src/components/UserSignOut.js
+++ client/src/components/UserSignOut.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import '../styles/index.css'
-function UserSignOut(){
-     
+import React from 'react'
+import { Redirect } from 'react-router-dom'
+
+export default ({ context }) => {
+  context.actions.signOut()
   return (
-
+    <Redirect to='/' />
   )
-
 }
-export default UserSignOut

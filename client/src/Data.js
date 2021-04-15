15ab427f0fb544a686a54aa68997fd85bbd7c101
tpo-aeclarke <aeclarke@travelers.com>
2021-04-15 13:40:22 -0400

    fixed compile error

--- client/src/Data.js
+++ client/src/Data.js
@@ -25,7 +25,7 @@ export default class Data {
   }
 
   async getUser (emailAddress, password) {
-    const response = await this.api('/users, 'GET', null, true, { emailAddress, password })
+    const response = await this.api('/users', 'GET', null, true, { emailAddress, password })
     if (response.status === 200) {
       return response.json().then(data => data)
     } else if (response.status === 401) {
